fc4cd1cad731b2899c692680aad56d205626db60
remigourdon <gourdon.remi@gmail.com>
2020-04-05 20:48:21 -0400

    Set git local config for external repos

--- run_once_setup-repos.sh
+++ run_once_setup-repos.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-# Configure terminfo for tmux
+# Configure external repositories
 set -Eeuo pipefail
 
-# Install scripts
-if ! [ -e "${HOME}/.scripts" ] ; then
-    git clone git@github.com:remigourdon/scripts.git "${HOME}/.scripts"
-fi
+config_local() {
+    local dest="${1}"
+    (
+        cd "${dest}"
+        git config --local user.name remigourdon
+        git config --local user.email gourdon.remi@gmail.com
+    )
+}
 
-# Install cheats
-if ! [ -e "${HOME}/.cheats" ] ; then
-    git clone git@github.com:remigourdon/cheats.git "${HOME}/.cheats"
-fi
+clone_repo() {
+    local repo="${1}"
+    local dest="${2}"
+    if [ -e "${dest}" ] ; then
+        return 1
+    fi
+    echo "Configuring ${repo} in ${dest}"
+    git clone "${repo}" "${dest}" && config_local "${dest}"
+}
 
-# Install snippets
-if ! [ -e "${HOME}/.snippets" ] ; then
-    git clone git@github.com:remigourdon/snippets.git "${HOME}/.snippets"
-fi
+config_local "$(chezmoi source-path)"
+
+declare -A REPOS
+REPOS["git@github.com:remigourdon/scripts.git"]="${HOME}/.scripts"
+REPOS["git@github.com:remigourdon/cheats.git"]="${HOME}/.cheats"
+REPOS["git@github.com:remigourdon/snippets.git"]="${HOME}/.snippets"
+
+for repo in "${!REPOS[@]}" ; do
+    clone_repo "${repo}" "${REPOS[${repo}]}" || true
+done
