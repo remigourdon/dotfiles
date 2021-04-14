a4e79d3aea8f54948dd4afbdd6286fc5db80b4bd
remigourdon <gourdon.remi@gmail.com>
2021-04-14 10:33:36 -0400

    Modify rofi tmux script to attach/kill sessions directly

--- private_dot_config/rofi/scripts/executable_tmux.sh
+++ private_dot_config/rofi/scripts/executable_tmux.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-# Rofi script to list tmux scripts and take action
+# Rofi script to list tmux session and attach or kill
 set -u # Treat unset variables and parameters as errors
 
 # Script selection
-LIST="$(fd '.*.sh' ~/.config/tmux/)"
-LAUNCHER="rofi -dmenu -i -p script -format d"
-FILTERED=$(echo "${LIST}" \
-    | sed -e "s!.*/tmux/\(.*\)\.sh!\1!gi" \
-    | ${LAUNCHER})
-
-# Action selection
-OPTIONS="Start session\nKill session"
-LAUNCHER="rofi -dmenu -i -p action -format d"
-if ! OPTION=$(echo -e "${OPTIONS}" | ${LAUNCHER}) ; then
+if ! SESSION_NAME="$(tmux list-sessions -F '#S' | rofi -dmenu -i -p script)" ; then
     exit 1
 fi
 
-SCRIPT="$(echo "${LIST}" | sed -n "${FILTERED}p")"
-echo "${SCRIPT}"
+# Action selection
+OPTIONS="Attach session\nKill session"
+LAUNCHER="rofi -dmenu -i -p action -format d"
+if ! OPTION=$(echo -e "${OPTIONS}" | ${LAUNCHER}) ; then
+    exit 2
+fi
 
 case ${OPTION} in
-    1) st -e "${SCRIPT}" ;;
-    2) ${SCRIPT} -k ;;
+    1) "${TERMINAL}" -e "bash" -c "tmux attach -t ${SESSION_NAME}" ;;
+    2) tmux kill-session -t "${SESSION_NAME}" ;;
     *) exit 1 ;;
 esac
