f8cc9ff70f6ef5a573e94a742d1ec2ee4dfa923e
remigourdon <gourdon.remi@gmail.com>
2022-04-02 21:03:06 -0400

    Import aliases as fish abbreviations

---
+++ private_dot_config/fish/conf.d/abbr.fish
@@ -0,0 +1,49 @@
+# Verbose for safety
+abbr -ga rm 'rm -i'
+abbr -ga cp 'cp -i'
+abbr -ga mv 'mv -i'
+
+# lsblk
+abbr -ga lk 'lsblk'
+
+# grep
+abbr -ga grep 'grep --color auto'
+
+# diff
+abbr -ga diff 'diff --unified --color auto'
+
+# git
+abbr -ga gs 'git status'
+abbr -ga gl 'git log --all --decorate --oneline --graph'
+abbr -ga gd 'git diff'
+abbr -ga ga 'git add'
+abbr -ga gaa 'git add .'
+abbr -ga gap 'git add --patch'
+abbr -ga gb 'git branch'
+abbr -ga gc 'git commit'
+abbr -ga gco 'git checkout'
+abbr -ga gco 'git clean'
+abbr -ga gcm 'git commit --message'
+abbr -ga gca 'git commit --amend'
+abbr -ga gri 'git rebase --interactive'
+
+# tmux
+abbr -ga tml 'tmux list-sessions'
+abbr -ga tma 'tmux attach-session -t'
+abbr -ga tmk 'tmux kill-session -t'
+abbr -ga tmn 'tmux new-session -A -s'
+
+# udiskie
+abbr -ga udm 'udiskie-mount --recursive'
+abbr -ga udu 'udiskie-umount --force'
+abbr -ga udua 'udiskie-umount --all'
+abbr -ga udd 'udiskie-umount --force --detach'
+abbr -ga udda 'udiskie-umount --all --detach'
+
+# Docker
+abbr -ga dkps 'docker ps --format "table {{.Names}}\\t{{.Image}}\\t{{.Status}}\\t{{.RunningFor}}"'
+abbr -ga dkrm 'docker rm --force'
+abbr -ga dkrmi 'docker rmi'
+abbr -ga dkls 'docker container ls'
+abbr -ga dklsi 'docker image ls'
+abbr -ga dkterm 'docker exec --tty --interactive'
