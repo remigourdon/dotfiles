592013d4df0afa457e4fd2db64d4320735b885f1
remigourdon <gourdon.remi@gmail.com>
2022-04-02 20:49:23 -0400

    Make greeting variable global

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,7 +2,7 @@
 fish_add_path $HOME/.fzf/bin
 
 if status is-interactive
-    set -U fish_greeting
+    set -g fish_greeting
     [ -f ~/.fzf/shell/key-bindings.fish ] && source ~/.fzf/shell/key-bindings.fish
     starship init fish | source
 end
