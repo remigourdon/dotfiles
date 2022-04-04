3dbd39b1411d021c62da938fa7f431b2717952fc
Rémi Gourdon <gourdon.remi@gmail.com>
2022-04-04 09:01:17 -0400

    Fish shell and Starship prompt (#2)
    * Use starship prompt with bash and zsh
    
    * Add default fish config with starship prompt and fzf
    
    * Set default shell for foot to fish
    
    * Add fzf to path for fish
    
    * Make greeting variable global
    
    * Import aliases as fish abbreviations
    
    * Add comments on fish config
    
    * Configure vi mode for fish
    
    * Add fish functions for fasd_cd and z
    
    * Add abbreviations for ls and exa
    
    * Add LICENSE file to chezmoi ignore

---
+++ private_dot_config/fish/config.fish
@@ -0,0 +1,15 @@
+# Add FZF to path
+fish_add_path $HOME/.fzf/bin
+
+if status is-interactive
+
+    # Disable fish greeting
+    set -g fish_greeting
+
+    # Load FZF keybindings function
+    [ -f ~/.fzf/shell/key-bindings.fish ] && source ~/.fzf/shell/key-bindings.fish
+
+    # Use starship for the prompt
+    starship init fish | source
+
+end
