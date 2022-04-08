e7e4fbf2d6101dc3bf5102a379bb2a93835f1fb6
remigourdon <gourdon.remi@gmail.com>
2022-04-08 18:12:58 -0400

    Improve fasd config based on fishgretel/fasd

--- private_dot_config/fish/functions/z.fish
+++ private_dot_config/fish/functions/z.fish
@@ -1,8 +1,7 @@
-function z -d 'Jump to directory using fasd and fzf'
+function z -d "Change directory to first match"
     if count $argv > 0
         fasd_cd -d "$argv"
     else
         cd (command fasd -Rdl $argv[1] | fzf -1 -0 --no-sort +m)
     end
 end
-
