e212114645a201fcc871c467f3cd6a4646f69276
remigourdon <gourdon.remi@gmail.com>
2022-04-11 18:15:07 -0400

    Set default fzf options for bash, zsh and fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -7,7 +7,26 @@ if status is-interactive
     set -g fish_greeting
 
     # Load FZF keybindings function
-    [ -f ~/.fzf/shell/key-bindings.fish ] && source ~/.fzf/shell/key-bindings.fish
+    if [ -f ~/.fzf/shell/key-bindings.fish ]
+        source ~/.fzf/shell/key-bindings.fish
+        if command -v fd &>/dev/null
+            set -gx FZF_DEFAULT_COMMAND "fd"
+            set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+            set -gx FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND --type d"
+        end
+        set -gx FZF_DEFAULT_OPTS "
+          --layout=reverse
+          --info=inline
+          --height=80%
+          --prompt='∼ ' --pointer='▶' --marker='✓'
+          --bind '?:toggle-preview'
+          --bind 'ctrl-a:select-all'
+          --bind 'ctrl-alt-a:deselect-all'
+          --bind 'ctrl-y:execute-silent(echo {+} | wl-copy)'
+          --bind 'ctrl-e:execute(echo {+} | xargs -o vim)'
+          --bind 'ctrl-v:execute(code {+})'
+        "
+    end
 
     # Enable asdf
     if [ -d "$HOME/.asdf" ]
